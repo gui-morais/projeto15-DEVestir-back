01a3d3990f0f1b6b49d943a2d4276a22e38e9759
Guilherme Morais <guilherme.morais0111@gmail.com>
2022-11-23 21:58:27 -0300

    alter port using environmental variables

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+// app.use(router);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}!`));
