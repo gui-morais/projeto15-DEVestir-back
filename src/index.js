12b68754879990925765e550e7c497305066da74
Guilherme Morais <guilherme.morais0111@gmail.com>
2022-11-24 10:37:05 -0300

    feat: create sign in router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import router from "./routes/routes";
+import router from "./routes/routes.js";
 
 const app = express();
 app.use(express.json());
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { signUp } from "../controllers/clients/signUp";
-import { validateNewClient } from "../middlewares/validateNewClient";
+import { signIn } from "../controllers/clients/signIn.js";
+import { signUp } from "../controllers/clients/signUp.js";
+import { validateClient } from "../middlewares/validateClient.js";
+import { validateNewClient } from "../middlewares/validateNewClient.js";
 
 const router = express.Router();
 router.post("/sign-up", validateNewClient, signUp);
+router.post("/sign-in", validateClient, signIn);
 
 export default router;
