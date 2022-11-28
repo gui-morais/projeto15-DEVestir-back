c3691f4720f9ec73f9e455799bf6e251082b0410
HugoNicolau <nicolau.hugogiles@gmail.com>
2022-11-28 01:45:50 -0300

    Feat: Showing the last items first

--- src/controllers/products/getProducts.js
+++ src/controllers/products/getProducts.js
@@ -2,7 +2,7 @@ import { productsCollection } from "../../database/db.js";
 
 export async function getProducts(req, res) {
     try{
-        const products = await productsCollection.find({}).toArray();
+        const products = await (await productsCollection.find({}).toArray()).reverse();
         return res.status(200).send(products);
     } catch(err) {
         console.log(err);
